lexesjan/gem5

cpu,arch: Put the name of the RegClass into the RegClass.
Move the name of the RegClass out of constants which belong to the
RegId, and instead store them in the RegClass instances.

Change-Id: I1ddd4bc8467d5e3f178db7a11c8f8052f43fd7ec

src/cpu/reg_class.hh
@@ -86,6 +86,7 @@ class RegClass
 {
   private:
     RegClassType _type;
+    const char *_name;
 
     size_t _size;
     size_t _regBytes = sizeof(RegVal);
@@ -99,9 +100,9 @@ class RegClass
     const debug::Flag &debugFlag;
 
   public:
-    constexpr RegClass(RegClassType type, size_t new_size,
-            const debug::Flag &debug_flag) :
-        _type(type), _size(new_size), debugFlag(debug_flag)
+    constexpr RegClass(RegClassType type, const char *new_name,
+            size_t new_size, const debug::Flag &debug_flag) :
+        _type(type), _name(new_name), _size(new_size), debugFlag(debug_flag)
     {}
 
     constexpr RegClass
@@ -123,6 +124,7 @@ class RegClass
     }
 
     constexpr RegClassType type() const { return _type; }
+    constexpr const char *name() const { return _name; }
     constexpr size_t size() const { return _size; }
     constexpr size_t regBytes() const { return _regBytes; }
     constexpr size_t regShift() const { return _regShift; }
@@ -144,7 +146,7 @@ class RegClass
 };
 
 inline constexpr RegClass
-    invalidRegClass(InvalidRegClass, 0, debug::InvalidReg);
+    invalidRegClass(InvalidRegClass, "invalid", 0, debug::InvalidReg);
 
 /** Register ID: describe an architectural register with its class and index.
  * This structure is used instead of just the register index to disambiguate
@@ -154,7 +156,6 @@ inline constexpr RegClass
 class RegId
 {
   protected:
-    static const char* regClassStrings[];
     const RegClass *_regClass = nullptr;
     RegIndex regIdx;
     int numPinnedWrites;
@@ -223,7 +224,7 @@ class RegId
     constexpr const char*
     className() const
     {
-        return regClassStrings[classValue()];
+        return _regClass->name();
     }
 
     int getNumPinnedWrites() const { return numPinnedWrites; }

TheV1rtuoso/gem5-rowhammer

src/cpu/reg_class.hh
@@ -86,6 +86,7 @@ class RegClass
 {
   private:
     RegClassType _type;
+    const char *_name;
 
     size_t _numRegs;
     size_t _regBytes = sizeof(RegVal);
@@ -99,9 +100,9 @@ class RegClass
     const debug::Flag &debugFlag;
 
   public:
-    constexpr RegClass(RegClassType type, size_t num_regs,
-            const debug::Flag &debug_flag) :
-        _type(type), _numRegs(num_regs), debugFlag(debug_flag)
+    constexpr RegClass(RegClassType type, const char *new_name,
+            size_t num_regs, const debug::Flag &debug_flag) :
+        _type(type), _name(new_name), _numRegs(num_regs), debugFlag(debug_flag)
     {}
 
     constexpr RegClass
@@ -123,6 +124,7 @@ class RegClass
     }
 
     constexpr RegClassType type() const { return _type; }
+    constexpr const char *name() const { return _name; }
     constexpr size_t numRegs() const { return _numRegs; }
     constexpr size_t regBytes() const { return _regBytes; }
     constexpr size_t regShift() const { return _regShift; }
@@ -144,7 +146,7 @@ class RegClass
 };
 
 inline constexpr RegClass
-    invalidRegClass(InvalidRegClass, 0, debug::InvalidReg);
+    invalidRegClass(InvalidRegClass, "invalid", 0, debug::InvalidReg);
 
 /** Register ID: describe an architectural register with its class and index.
  * This structure is used instead of just the register index to disambiguate
@@ -154,7 +156,6 @@ inline constexpr RegClass
 class RegId
 {
   protected:
-    static const char* regClassStrings[];
     const RegClass *_regClass = nullptr;
     RegIndex regIdx;
     int numPinnedWrites;
@@ -223,7 +224,7 @@ class RegId
     constexpr const char*
     className() const
     {
-        return regClassStrings[classValue()];
+        return _regClass->name();
     }
 
     int getNumPinnedWrites() const { return numPinnedWrites; }