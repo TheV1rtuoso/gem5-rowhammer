lexesjan/gem5

cpu,arch: Attach a debug flag to each RegClass.
This can be used for DPRINTFs related to those registers using DPRINTFV.

Change-Id: I0fccb12b70fdb74e01022fe0d3d9c2f92425a5bf

src/cpu/reg_class.hh
@@ -46,6 +46,7 @@
 #include <string>
 
 #include "base/cprintf.hh"
+#include "base/debug.hh"
 #include "base/intmath.hh"
 #include "base/types.hh"
 
@@ -88,16 +89,18 @@ class RegClass
 
     static inline RegClassOps defaultOps;
     RegClassOps *_ops = &defaultOps;
+    const debug::Flag &debugFlag;
 
   public:
-    RegClass(size_t new_size, RegIndex new_zero=-1,
-            size_t reg_bytes=sizeof(RegVal)) :
+    RegClass(size_t new_size, const debug::Flag &debug_flag,
+            RegIndex new_zero=-1, size_t reg_bytes=sizeof(RegVal)) :
         _size(new_size), _zeroReg(new_zero), _regBytes(reg_bytes),
-        _regShift(ceilLog2(reg_bytes))
+        _regShift(ceilLog2(reg_bytes)), debugFlag(debug_flag)
     {}
-    RegClass(size_t new_size, RegClassOps &new_ops, RegIndex new_zero=-1,
+    RegClass(size_t new_size, RegClassOps &new_ops,
+            const debug::Flag &debug_flag, RegIndex new_zero=-1,
             size_t reg_bytes=sizeof(RegVal)) :
-        RegClass(new_size, new_zero, reg_bytes)
+        RegClass(new_size, debug_flag, new_zero, reg_bytes)
     {
         _ops = &new_ops;
     }
@@ -106,6 +109,7 @@ class RegClass
     RegIndex zeroReg() const { return _zeroReg; }
     size_t regBytes() const { return _regBytes; }
     size_t regShift() const { return _regShift; }
+    const debug::Flag &debug() const { return debugFlag; }
 
     std::string regName(const RegId &id) const { return _ops->regName(id); }
     std::string

TheV1rtuoso/gem5-rowhammer

src/cpu/reg_class.hh
@@ -46,6 +46,7 @@
 #include <string>
 
 #include "base/cprintf.hh"
+#include "base/debug.hh"
 #include "base/intmath.hh"
 #include "base/types.hh"
 
@@ -88,16 +89,18 @@ class RegClass
 
     static inline RegClassOps defaultOps;
     RegClassOps *_ops = &defaultOps;
+    const debug::Flag &debugFlag;
 
   public:
-    RegClass(size_t num_regs, RegIndex new_zero=-1,
-            size_t reg_bytes=sizeof(RegVal)) :
+    RegClass(size_t num_regs, const debug::Flag &debug_flag,
+            RegIndex new_zero=-1, size_t reg_bytes=sizeof(RegVal)) :
         _numRegs(num_regs), _zeroReg(new_zero), _regBytes(reg_bytes),
-        _regShift(ceilLog2(reg_bytes))
+        _regShift(ceilLog2(reg_bytes)), debugFlag(debug_flag)
     {}
-    RegClass(size_t num_regs, RegClassOps &new_ops, RegIndex new_zero=-1,
+    RegClass(size_t num_regs, RegClassOps &new_ops,
+            const debug::Flag &debug_flag, RegIndex new_zero=-1,
             size_t reg_bytes=sizeof(RegVal)) :
-        RegClass(num_regs, new_zero, reg_bytes)
+        RegClass(num_regs, debug_flag, new_zero, reg_bytes)
     {
         _ops = &new_ops;
     }
@@ -106,6 +109,7 @@ class RegClass
     RegIndex zeroReg() const { return _zeroReg; }
     size_t regBytes() const { return _regBytes; }
     size_t regShift() const { return _regShift; }
+    const debug::Flag &debug() const { return debugFlag; }
 
     std::string regName(const RegId &id) const { return _ops->regName(id); }
     std::string